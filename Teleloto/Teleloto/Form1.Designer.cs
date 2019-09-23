bfd9b5562a988bcc50d92a4dd787c6859eb4d494
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-23 19:48:34 +0300

    Pilnai padarytas teleloto zaidimas

---
+++ Teleloto/Teleloto/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Teleloto
+{
+    partial class TelelotoZaidimas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TelelotoZaidimas));
+            this.GroupBilietas = new System.Windows.Forms.GroupBox();
+            this.GeneruotiMygtukas = new System.Windows.Forms.Button();
+            this.Z5 = new System.Windows.Forms.TextBox();
+            this.Z4 = new System.Windows.Forms.TextBox();
+            this.Z3 = new System.Windows.Forms.TextBox();
+            this.Z2 = new System.Windows.Forms.TextBox();
+            this.Z1 = new System.Windows.Forms.TextBox();
+            this.R5 = new System.Windows.Forms.TextBox();
+            this.R4 = new System.Windows.Forms.TextBox();
+            this.R3 = new System.Windows.Forms.TextBox();
+            this.R2 = new System.Windows.Forms.TextBox();
+            this.R1 = new System.Windows.Forms.TextBox();
+            this.G5 = new System.Windows.Forms.TextBox();
+            this.G4 = new System.Windows.Forms.TextBox();
+            this.G3 = new System.Windows.Forms.TextBox();
+            this.G2 = new System.Windows.Forms.TextBox();
+            this.G1 = new System.Windows.Forms.TextBox();
+            this.J5 = new System.Windows.Forms.TextBox();
+            this.J4 = new System.Windows.Forms.TextBox();
+            this.J3 = new System.Windows.Forms.TextBox();
+            this.J2 = new System.Windows.Forms.TextBox();
+            this.J1 = new System.Windows.Forms.TextBox();
+            this.M5 = new System.Windows.Forms.TextBox();
+            this.M4 = new System.Windows.Forms.TextBox();
+            this.M3 = new System.Windows.Forms.TextBox();
+            this.M2 = new System.Windows.Forms.TextBox();
+            this.M1 = new System.Windows.Forms.TextBox();
+            this.ZaidimasGroup = new System.Windows.Forms.GroupBox();
+            this.ZaistiB = new System.Windows.Forms.Button();
+            this.KamuoliukaiIsridenti = new System.Windows.Forms.TextBox();
+            this.GroupBilietas.SuspendLayout();
+            this.ZaidimasGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // GroupBilietas
+            // 
+            this.GroupBilietas.Controls.Add(this.GeneruotiMygtukas);
+            this.GroupBilietas.Controls.Add(this.Z5);
+            this.GroupBilietas.Controls.Add(this.Z4);
+            this.GroupBilietas.Controls.Add(this.Z3);
+            this.GroupBilietas.Controls.Add(this.Z2);
+            this.GroupBilietas.Controls.Add(this.Z1);
+            this.GroupBilietas.Controls.Add(this.R5);
+            this.GroupBilietas.Controls.Add(this.R4);
+            this.GroupBilietas.Controls.Add(this.R3);
+            this.GroupBilietas.Controls.Add(this.R2);
+            this.GroupBilietas.Controls.Add(this.R1);
+            this.GroupBilietas.Controls.Add(this.G5);
+            this.GroupBilietas.Controls.Add(this.G4);
+            this.GroupBilietas.Controls.Add(this.G3);
+            this.GroupBilietas.Controls.Add(this.G2);
+            this.GroupBilietas.Controls.Add(this.G1);
+            this.GroupBilietas.Controls.Add(this.J5);
+            this.GroupBilietas.Controls.Add(this.J4);
+            this.GroupBilietas.Controls.Add(this.J3);
+            this.GroupBilietas.Controls.Add(this.J2);
+            this.GroupBilietas.Controls.Add(this.J1);
+            this.GroupBilietas.Controls.Add(this.M5);
+            this.GroupBilietas.Controls.Add(this.M4);
+            this.GroupBilietas.Controls.Add(this.M3);
+            this.GroupBilietas.Controls.Add(this.M2);
+            this.GroupBilietas.Controls.Add(this.M1);
+            resources.ApplyResources(this.GroupBilietas, "GroupBilietas");
+            this.GroupBilietas.Name = "GroupBilietas";
+            this.GroupBilietas.TabStop = false;
+            // 
+            // GeneruotiMygtukas
+            // 
+            resources.ApplyResources(this.GeneruotiMygtukas, "GeneruotiMygtukas");
+            this.GeneruotiMygtukas.Name = "GeneruotiMygtukas";
+            this.GeneruotiMygtukas.UseVisualStyleBackColor = true;
+            this.GeneruotiMygtukas.Click += new System.EventHandler(this.GeneruotiMygtukas_Click);
+            // 
+            // Z5
+            // 
+            resources.ApplyResources(this.Z5, "Z5");
+            this.Z5.Name = "Z5";
+            // 
+            // Z4
+            // 
+            resources.ApplyResources(this.Z4, "Z4");
+            this.Z4.Name = "Z4";
+            // 
+            // Z3
+            // 
+            resources.ApplyResources(this.Z3, "Z3");
+            this.Z3.Name = "Z3";
+            // 
+            // Z2
+            // 
+            resources.ApplyResources(this.Z2, "Z2");
+            this.Z2.Name = "Z2";
+            // 
+            // Z1
+            // 
+            resources.ApplyResources(this.Z1, "Z1");
+            this.Z1.Name = "Z1";
+            // 
+            // R5
+            // 
+            resources.ApplyResources(this.R5, "R5");
+            this.R5.Name = "R5";
+            // 
+            // R4
+            // 
+            resources.ApplyResources(this.R4, "R4");
+            this.R4.Name = "R4";
+            // 
+            // R3
+            // 
+            resources.ApplyResources(this.R3, "R3");
+            this.R3.Name = "R3";
+            // 
+            // R2
+            // 
+            resources.ApplyResources(this.R2, "R2");
+            this.R2.Name = "R2";
+            // 
+            // R1
+            // 
+            resources.ApplyResources(this.R1, "R1");
+            this.R1.Name = "R1";
+            // 
+            // G5
+            // 
+            resources.ApplyResources(this.G5, "G5");
+            this.G5.Name = "G5";
+            // 
+            // G4
+            // 
+            resources.ApplyResources(this.G4, "G4");
+            this.G4.Name = "G4";
+            // 
+            // G3
+            // 
+            resources.ApplyResources(this.G3, "G3");
+            this.G3.Name = "G3";
+            // 
+            // G2
+            // 
+            resources.ApplyResources(this.G2, "G2");
+            this.G2.Name = "G2";
+            // 
+            // G1
+            // 
+            resources.ApplyResources(this.G1, "G1");
+            this.G1.Name = "G1";
+            // 
+            // J5
+            // 
+            resources.ApplyResources(this.J5, "J5");
+            this.J5.Name = "J5";
+            // 
+            // J4
+            // 
+            resources.ApplyResources(this.J4, "J4");
+            this.J4.Name = "J4";
+            // 
+            // J3
+            // 
+            resources.ApplyResources(this.J3, "J3");
+            this.J3.Name = "J3";
+            // 
+            // J2
+            // 
+            resources.ApplyResources(this.J2, "J2");
+            this.J2.Name = "J2";
+            // 
+            // J1
+            // 
+            resources.ApplyResources(this.J1, "J1");
+            this.J1.Name = "J1";
+            // 
+            // M5
+            // 
+            resources.ApplyResources(this.M5, "M5");
+            this.M5.Name = "M5";
+            // 
+            // M4
+            // 
+            resources.ApplyResources(this.M4, "M4");
+            this.M4.Name = "M4";
+            // 
+            // M3
+            // 
+            resources.ApplyResources(this.M3, "M3");
+            this.M3.Name = "M3";
+            // 
+            // M2
+            // 
+            resources.ApplyResources(this.M2, "M2");
+            this.M2.Name = "M2";
+            // 
+            // M1
+            // 
+            resources.ApplyResources(this.M1, "M1");
+            this.M1.Name = "M1";
+            // 
+            // ZaidimasGroup
+            // 
+            this.ZaidimasGroup.Controls.Add(this.KamuoliukaiIsridenti);
+            this.ZaidimasGroup.Controls.Add(this.ZaistiB);
+            resources.ApplyResources(this.ZaidimasGroup, "ZaidimasGroup");
+            this.ZaidimasGroup.Name = "ZaidimasGroup";
+            this.ZaidimasGroup.TabStop = false;
+            // 
+            // ZaistiB
+            // 
+            resources.ApplyResources(this.ZaistiB, "ZaistiB");
+            this.ZaistiB.Name = "ZaistiB";
+            this.ZaistiB.UseVisualStyleBackColor = true;
+            this.ZaistiB.Click += new System.EventHandler(this.ZaistiB_Click);
+            // 
+            // KamuoliukaiIsridenti
+            // 
+            resources.ApplyResources(this.KamuoliukaiIsridenti, "KamuoliukaiIsridenti");
+            this.KamuoliukaiIsridenti.Name = "KamuoliukaiIsridenti";
+            // 
+            // TelelotoZaidimas
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ZaidimasGroup);
+            this.Controls.Add(this.GroupBilietas);
+            this.Name = "TelelotoZaidimas";
+            this.GroupBilietas.ResumeLayout(false);
+            this.GroupBilietas.PerformLayout();
+            this.ZaidimasGroup.ResumeLayout(false);
+            this.ZaidimasGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox GroupBilietas;
+        private System.Windows.Forms.TextBox M1;
+        private System.Windows.Forms.TextBox M5;
+        private System.Windows.Forms.TextBox M4;
+        private System.Windows.Forms.TextBox M3;
+        private System.Windows.Forms.TextBox M2;
+        private System.Windows.Forms.Button GeneruotiMygtukas;
+        private System.Windows.Forms.TextBox Z5;
+        private System.Windows.Forms.TextBox Z4;
+        private System.Windows.Forms.TextBox Z3;
+        private System.Windows.Forms.TextBox Z2;
+        private System.Windows.Forms.TextBox Z1;
+        private System.Windows.Forms.TextBox R5;
+        private System.Windows.Forms.TextBox R4;
+        private System.Windows.Forms.TextBox R3;
+        private System.Windows.Forms.TextBox R2;
+        private System.Windows.Forms.TextBox R1;
+        private System.Windows.Forms.TextBox G5;
+        private System.Windows.Forms.TextBox G4;
+        private System.Windows.Forms.TextBox G3;
+        private System.Windows.Forms.TextBox G2;
+        private System.Windows.Forms.TextBox G1;
+        private System.Windows.Forms.TextBox J5;
+        private System.Windows.Forms.TextBox J4;
+        private System.Windows.Forms.TextBox J3;
+        private System.Windows.Forms.TextBox J2;
+        private System.Windows.Forms.TextBox J1;
+        private System.Windows.Forms.GroupBox ZaidimasGroup;
+        private System.Windows.Forms.TextBox KamuoliukaiIsridenti;
+        private System.Windows.Forms.Button ZaistiB;
+    }
+}
+
---
+++ Teleloto/Teleloto/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Teleloto
+{
+    public partial class TelelotoZaidimas : Form
+    {
+        public TelelotoZaidimas()
+        {
+            InitializeComponent();
+        }
+
+        private void GeneruotiMygtukas_Click(object sender, EventArgs e)
+        {
+            TextBox[] Melyni = { M1, M2, M3, M4, M5 };
+            SugeneruotiStulpeli(Melyni, 1, 16);
+            TextBox[] Juodi = { J1, J2, J3, J4, J5 };
+            SugeneruotiStulpeli(Juodi, 16, 31);
+            TextBox[] Raudoni = { R1, R2, R3, R4, R5 };
+            SugeneruotiStulpeli(Raudoni, 31, 46);
+            TextBox[] Geltoni = { G1, G2, G3, G4, G5 };
+            SugeneruotiStulpeli(Geltoni, 46, 61);
+            TextBox[] Zali = { Z1, Z2, Z3, Z4, Z5 };
+            SugeneruotiStulpeli(Zali, 61, 76);
+        }
+
+        private void SugeneruotiStulpeli(TextBox[] stulpelis, int a, int b)
+        {
+            Random rng = new Random();
+            int i = 0;
+            List<int> isridenti = new List<int>();
+            while (i < stulpelis.Length)
+            {
+                int kamuoliukas = rng.Next(a, b);
+
+                bool jauIsridentas = false;
+                foreach (var kamuoliuka in isridenti)
+                {
+                    if (kamuoliuka == kamuoliukas)
+                    {
+                        jauIsridentas = true;
+                        break;
+                    }
+                }
+
+                if (!jauIsridentas)
+                {
+                    stulpelis[i].Text = kamuoliukas.ToString();
+                    i++;
+                    isridenti.Add(kamuoliukas);
+                }
+            }
+        }
+
+        private void ZaistiB_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+            Random rng = new Random();
+            List<int> IsridentiKamuoliukaiAuksiniui = new List<int>();
+            while (i < 45)
+            {
+                int kamuoliukas = rng.Next(1, 76);
+                bool jauIsridentas = false;
+
+                foreach (var kam in IsridentiKamuoliukaiAuksiniui)
+                {
+                    if (kam == kamuoliukas)
+                    {
+                        jauIsridentas = true;
+                        break;
+                    }
+                }
+
+                if (!jauIsridentas)
+                {
+                    KamuoliukaiIsridenti.Text += kamuoliukas.ToString() + " ";
+                    i++;
+                    PatirkintiKamuoliuka(kamuoliukas);
+                    IsridentiKamuoliukaiAuksiniui.Add(kamuoliukas);
+                }
+            }
+        }
+
+        private void PatirkintiKamuoliuka(int kamuoliukas)
+        {
+            TextBox[] bilietas = {M1, M2, M3, M4, M5,
+                                  J1, J2, J3, J4, J5,
+                                   R1, R2, R3, R4, R5,
+                                    G1, G2, G3, G4, G5,
+                                   Z1, Z2, Z3, Z4, Z5 };
+            foreach (var item in bilietas)
+            {
+                if (item.Text == kamuoliukas.ToString())
+                {
+                    item.BackColor = Color.Green;
+                    break;
+                }
+            }
+        }
+    }
+}
