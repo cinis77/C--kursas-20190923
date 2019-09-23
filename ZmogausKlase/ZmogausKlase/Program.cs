4c4710cb1ee4a6e76082d6fc3c9ded046f43c989
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-23 20:40:44 +0300

    su open file dialog

---
+++ ZmogausKlase/ZmogausKlase/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZmogausKlase
+{
+    internal class Program
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            string path = "";
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                }
+            }
+            List<Zmogus> zmones = new List<Zmogus>();
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(path))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var duomenys = line.Split(';');
+                    //Vardas;Pavarde;GimimoData;Lytis
+                    Zmogus zmogus = new Zmogus(duomenys[0], duomenys[1],
+                        DateTime.Parse(duomenys[2]), duomenys[3]);
+                    zmones.Add(zmogus);
+                }
+            }
+            Console.WriteLine(zmones.First().Vardas + " " + zmones.First().Pavarde +
+                " " + zmones.First().GimimoDiena + " " + zmones.First().Lytis);
+        }
+    }
+}
---
+++ ZmogausKlase/ZmogausKlase/Zmogus.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZmogausKlase
+{
+    internal class Zmogus
+    {
+        public string Vardas { get; }
+        public string Pavarde { get; private set; }
+        public DateTime GimimoDiena { get; }
+        public string Lytis { get; private set; }
+
+        public Zmogus(string Vardas, string Pavarde, DateTime GimimoData, string Lytis)
+        {
+            this.Vardas = Vardas;
+            this.Pavarde = Pavarde;
+            GimimoDiena = GimimoData;
+            this.Lytis = Lytis;
+        }
+
+        public bool ArSuages()
+        {
+            if (DateTime.Today.Year - GimimoDiena.Year >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
