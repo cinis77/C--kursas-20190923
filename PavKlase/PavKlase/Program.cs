98a501198ff5ce3e9e2d35b69b861c5d0b04b6e6
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-24 19:39:51 +0300

    Override ir anoniminiai objektai

---
+++ PavKlase/PavKlase/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PavKlase
+{
+    internal class Telefonas
+    {
+        public int Numeris { get; private set; }
+        public string Gamintojas { get; }
+        public DateTime GamybosMetai { get; }
+
+        public Telefonas(string Gamintojas, DateTime GamybosMetai)
+        {
+            this.Gamintojas = Gamintojas;
+            this.GamybosMetai = GamybosMetai;
+        }
+
+        public virtual void SuteiktiNumeri(int numeris)
+        {
+            Numeris = numeris;
+        }
+    }
+
+    internal class LaidinisTelefonas : Telefonas
+    {
+        public string IvedimoTipas { get; }
+
+        public string Tipas { get; set; }
+
+        public LaidinisTelefonas(string gamintojas, DateTime metai)
+            : base(GamybosMetai: metai, Gamintojas: gamintojas)
+        {
+        }
+
+        public LaidinisTelefonas(string gamintojas, DateTime gamybosMetai, string Tipas)
+            : base(gamintojas, gamybosMetai)
+        {
+        }
+
+        public override void SuteiktiNumeri(int numeris)
+        {
+            numeris = 837000000 + numeris;
+            base.SuteiktiNumeri(numeris);
+            Console.WriteLine("Esu laidinis telefonas");
+        }
+    }
+
+    internal class MobilusTelefonas : Telefonas
+    {
+        public string SimKortelesTipas { get; set; }
+        public string RysioTipas { get; set; }
+
+        public MobilusTelefonas(string gamintojas, DateTime gamybosMetai) : base(gamintojas, gamybosMetai)
+        {
+        }
+
+        public override void SuteiktiNumeri(int numeris)
+        {
+            numeris = 86000000 + numeris;
+            base.SuteiktiNumeri(numeris);
+        }
+    }
+
+    internal class IsmanusTelefonas : MobilusTelefonas
+    {
+        public string OperacineSistemosTipas { get; set; }
+        public string Kamera { get; set; }
+        public string Dizainas { get; set; }
+
+        public IsmanusTelefonas(string gamintojas, DateTime gamybosMetai)
+            : base(gamintojas, gamybosMetai)
+        {
+        }
+
+        public override void SuteiktiNumeri(int numeris)
+        {
+            base.SuteiktiNumeri(numeris);
+            Console.WriteLine("Esu ismanusis telefonas");
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<Telefonas> telefonai = new List<Telefonas>();
+            int sk = 5;
+            LaidinisTelefonas laidinis = new LaidinisTelefonas("Alkatel", DateTime.Today);
+            telefonai.Add(laidinis);
+            telefonai.Add(new LaidinisTelefonas("Siemens", new DateTime(1996, 08, 12)));
+            telefonai.Add(new MobilusTelefonas("Nokia", new DateTime(2009, 09, 05)));
+            telefonai.Add(new IsmanusTelefonas("Samsung", DateTime.Today));
+            foreach (var item in telefonai)
+            {
+                item.SuteiktiNumeri(355521);
+            }
+        }
+    }
+}
---
+++ PaveldinciosKlases/PaveldinciosKlases/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaveldinciosKlases
+{
+    internal class Gyvunas // Bazine klase
+    {
+        public string KailioTipas { get; set; }
+        public int Amzius { get; set; }
+
+        public Gyvunas(string kailioTipas)
+        {
+            KailioTipas = kailioTipas;
+        }
+    }
+
+    internal class Suo : Gyvunas //Paveldincio klases
+    {
+        public string TipasNamuos;
+
+        public Suo(string kailioTipo) : base(kailioTipo)
+        {
+        }
+    }
+
+    internal class Korgis : Suo
+    {
+        public string Vardas;
+
+        public Korgis(string kailioTipo) : base(kailioTipo)
+        {
+        }
+    }
+
+    internal class Kate : Gyvunas // Paveldinti klase
+    {
+        public string TingumoLygis;
+
+        public Kate(string kailis) : base(kailis)
+        {
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Gyvunas gyvunas = new Gyvunas("");
+            Suo suo = new Suo("");
+            gyvunas = suo; // Polimorfizmas
+
+            List<Gyvunas> gyvunai = new List<Gyvunas>();
+            gyvunai.Add(suo);
+            Kate kate = new Kate("");
+            gyvunai.Add(kate);
+
+            object objektas = suo;
+            double sk = 10.5;
+            int sk1 = (int)sk;
+            Suo sun = (Suo)objektas; //(tipas) kintamasis => kastinimu
+        }
+    }
+}
