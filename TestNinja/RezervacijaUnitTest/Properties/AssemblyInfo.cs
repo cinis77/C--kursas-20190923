ceeb33a29ba3bf66992cf349cfbfda7a3cafbbdf
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-26 19:36:26 +0300

    Visi testai pirmas pavyzdys

---
+++ TestNinja/RezervacijaUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RezervacijaUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RezervacijaUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db073c4d-697e-496c-afb3-27c1360acbb5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja/RezervacijaUnitTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja;
+
+namespace RezervacijaUnitTest
+{
+    [TestClass]
+    public class RezervacijaUnitTest
+    {
+        [TestMethod]
+        public void AtsauktiRezervacija_UserIsAdmin_ReturnTrue()
+        {
+            //Arange
+            Rezervacija rezervacija = new Rezervacija();
+            //Act
+            var result =
+                rezervacija.AtsauktiRezervacija(new User() { IsAdmin = true });
+            //Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AtsauktiRezervacija_AtsaukiaUzsakinetojas_ReturnTrue()
+        {
+            User user = new User();
+            Rezervacija rezervacija = new Rezervacija() { KienoRezervacija = user };
+
+            var result = rezervacija.AtsauktiRezervacija(user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void AtsauktiRezervacija_AtsaukiaNeUzsakinetojas_ReturnFalse()
+        {
+            Rezervacija rezervacija = new Rezervacija()
+            { KienoRezervacija = new User() };
+
+            var result =
+                rezervacija.AtsauktiRezervacija(new User() { IsAdmin = false });
+
+            Assert.IsFalse(result);
+        }
+    }
+}
