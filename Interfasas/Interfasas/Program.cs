5073be3aeb751230e281b19769de75f0f7a81e7e
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-26 19:11:06 +0300

    Interface ir testavimas

---
+++ Interfasas/Interfasas/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfasas
+{
+    internal interface Ainterfas
+    {
+        void MetodasA();
+
+        int PropertisA { get; set; }
+    }
+
+    internal interface Binterfas
+    {
+        void MetodasB();
+
+        int PropertisB { get; set; }
+    }
+
+    internal class ManoKlase : Ainterfas, Binterfas
+    {
+        private int _A;
+
+        public int PropertisA
+        {
+            get
+            {
+                return _A;
+            }
+            set
+            {
+                if (value != _A)
+                {
+                    _A = value;
+                }
+            }
+        }
+
+        public int PropertisB { get; set; }
+
+        public void MetodasA()
+        {
+            Console.WriteLine("Panaudojamas metodas pirma klase");
+        }
+
+        public void MetodasB()
+        {
+            Console.WriteLine("Panaudojamas metodas pirma klase");
+        }
+    }
+
+    internal class AntraKlase
+    {
+        public Ainterfas InterfaceProperty { get; set; }
+
+        public AntraKlase(Ainterfas ainterfas)
+        {
+            InterfaceProperty = ainterfas;
+            InterfaceProperty.MetodasA();
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ManoKlase mano = new ManoKlase();
+            AntraKlase antra = new AntraKlase(mano);
+        }
+    }
+}
---
+++ TestNinja/TestNinja/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja
+{
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+
+    public class Rezervacija
+    {
+        public User KienoRezervacija { get; set; }
+
+        public void DarytRezervacija(User uzsakytojas)
+        {
+            KienoRezervacija = uzsakytojas;
+        }
+
+        public bool AtsauktiRezervacija(User atsaukejas)
+        {
+            if (atsaukejas == KienoRezervacija)
+            {
+                return true;
+            }
+            else if (atsaukejas.IsAdmin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
