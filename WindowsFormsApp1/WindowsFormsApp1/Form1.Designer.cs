20fe95044707d1d48ecf6654b73c112e5efe2d4f
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-09-30 20:47:00 +0300

    UI su tabais

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Tabai = new System.Windows.Forms.TabControl();
+            this.PirmasTab = new System.Windows.Forms.TabPage();
+            this.NewTab = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Tabai.SuspendLayout();
+            this.PirmasTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Tabai
+            // 
+            this.Tabai.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.Tabai.Controls.Add(this.PirmasTab);
+            this.Tabai.Controls.Add(this.NewTab);
+            this.Tabai.ImeMode = System.Windows.Forms.ImeMode.Hiragana;
+            this.Tabai.Location = new System.Drawing.Point(35, 23);
+            this.Tabai.Name = "Tabai";
+            this.Tabai.SelectedIndex = 0;
+            this.Tabai.Size = new System.Drawing.Size(1870, 900);
+            this.Tabai.SizeMode = System.Windows.Forms.TabSizeMode.Fixed;
+            this.Tabai.TabIndex = 0;
+            this.Tabai.SelectedIndexChanged += new System.EventHandler(this.Tabai_SelectedIndexChanged);
+            // 
+            // PirmasTab
+            // 
+            this.PirmasTab.BackColor = System.Drawing.SystemColors.Control;
+            this.PirmasTab.Controls.Add(this.button1);
+            this.PirmasTab.Location = new System.Drawing.Point(4, 4);
+            this.PirmasTab.Name = "PirmasTab";
+            this.PirmasTab.Padding = new System.Windows.Forms.Padding(3);
+            this.PirmasTab.Size = new System.Drawing.Size(1862, 863);
+            this.PirmasTab.TabIndex = 0;
+            this.PirmasTab.Text = "Pirmas";
+            // 
+            // NewTab
+            // 
+            this.NewTab.Location = new System.Drawing.Point(4, 4);
+            this.NewTab.Name = "NewTab";
+            this.NewTab.Padding = new System.Windows.Forms.Padding(3);
+            this.NewTab.Size = new System.Drawing.Size(1862, 863);
+            this.NewTab.TabIndex = 1;
+            this.NewTab.Text = "Naujas";
+            this.NewTab.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(114, 91);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(224, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1984, 1008);
+            this.Controls.Add(this.Tabai);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Tabai.ResumeLayout(false);
+            this.PirmasTab.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Tabai;
+        private System.Windows.Forms.TabPage PirmasTab;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabPage NewTab;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            string path = null;
+            ofd.Filter = "Paveiksliukas mazas *.png|*.png| Internetinis *.jpg|*.jpg|All files|*.*";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                path = ofd.FileName;
+            }
+            PictureBox picture = new PictureBox();
+            picture.BackgroundImage = Image.FromFile(path);
+            picture.Size =
+                new Size(Tabai.SelectedTab.Size.Width - 20, Tabai.SelectedTab.Size.Height - 20);
+            picture.BackgroundImageLayout = ImageLayout.Stretch;
+            foreach (Control item in Tabai.SelectedTab.Controls)
+            {
+                if (item is Button)
+                {
+                    Tabai.SelectedTab.Controls.Remove(item);
+                }
+            }
+            Tabai.SelectedTab.Controls.Add(picture);
+        }
+
+        private void Tabai_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (Tabai.SelectedTab == NewTab)
+            {
+                Tabai.TabPages.Remove(NewTab);
+                TabPage tabpage = new TabPage();
+                Button mygtukas = new Button();
+                mygtukas.Text = button1.Text;
+                mygtukas.Size = button1.Size;
+                mygtukas.Location = button1.Location;
+                mygtukas.Click += button1_Click;
+                tabpage.Controls.Add(mygtukas);
+                Tabai.TabPages.Add(tabpage);
+                Tabai.TabPages.Add(NewTab);
+                Tabai.SelectedTab = tabpage;
+            }
+        }
+    }
+}
