ec95a63409cc3f76f8c14369477ac38fe2c8cdbc
cinis77 <cinis.dwarnius.minis@gmail.com>
2019-10-17 18:50:46 +0300

    Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class MusuSingleton
+    {
+        private MusuSingleton()
+        {
+        }
+
+        public static int SudetiDuSkaicius(int a, int b)
+        {
+            return a + b;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(MusuSingleton.SudetiDuSkaicius(1, 1));
+        }
+    }
+}
